b0a0e66f8c899b72bb5c8d1e2202e712575a2315
Jo JungRae <jowjdfo@gmail.com>
2025-05-08 02:04:45 +0900

    조이스틱 이동 구현 및 firebase 구축
    - Joystick 클래스 추가
    - Joystick의 입력을 전달하는 JoystickInputHandler 클래스 추가
    - PlayerInputHandler 수정 : 조이스틱을 자동으로 감지하고, 조이스틱 입력을 전달받아 캐릭터를 조작할 수 있는 기능 추가

--- Assets/Script/Player/Player_Move/CharMove/PlayerInputHandler.cs
+++ Assets/Script/Player/Player_Move/CharMove/PlayerInputHandler.cs
@@ -8,6 +8,8 @@ public class PlayerInputHandler : MonoBehaviour
     private bool isMoving = false;
     private Rigidbody rb;
 
+    [SerializeField] private JoystickController joystick;
+    
     void Start()
     {
         keyboard = Keyboard.current;
@@ -15,7 +17,13 @@ public class PlayerInputHandler : MonoBehaviour
         {
             Debug.LogError("키보드를 찾을 수 없습니다!");
         }
-        
+
+        if (joystick == null)
+        {
+            joystick = FindObjectOfType<JoystickController>();
+            Debug.LogError("조이스틱을 찾을 수 없습니다!");
+        }
+
         rb = GetComponent<Rigidbody>();
     }
 
@@ -27,18 +35,29 @@ public class PlayerInputHandler : MonoBehaviour
         moveDirection = 0f;
         isMoving = false;
         
-        // 왼쪽 이동 (A 키)
-        if (keyboard.aKey.isPressed && rb != null && !rb.isKinematic)
+        // 조이스틱 입력 감지
+        if (joystick != null && joystick.IsMoving())
         {
-            moveDirection = -1f;
+            moveDirection = joystick.GetHorizontal();
             isMoving = true;
         }
-        
-        // 오른쪽 이동 (D 키)
-        if (keyboard.dKey.isPressed && rb != null && !rb.isKinematic)
-        {
-            moveDirection = 1f;
-            isMoving = true;
+
+        // 키 입력 감지
+        // 왼쪽 이동 (A 키)
+        else if (keyboard != null && !rb.isKinematic)
+        { 
+            if (keyboard.aKey.isPressed && rb != null && !rb.isKinematic)
+            {
+                moveDirection = -1f;
+                isMoving = true;
+            }
+            
+            // 오른쪽 이동 (D 키)
+            if (keyboard.dKey.isPressed && rb != null && !rb.isKinematic)
+            {
+                moveDirection = 1f;
+                isMoving = true;
+            }
         }
     }
 
