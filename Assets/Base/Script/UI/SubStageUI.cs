79ddd167a5cc5a53766b3ab1a8a090aea8d8170f
윤진환 <www139455@naver.com>
2025-06-01 03:38:36 +0900

    [fix] - 씬 연결 얼추 완료
    이제 마무리는 팀원이랑 맞춰보면 되거나 버그 픽스 정도

--- Assets/Base/Script/UI/SubStageUI.cs
+++ Assets/Base/Script/UI/SubStageUI.cs
@@ -16,7 +16,7 @@ public class SubStageUI : MonoBehaviour
             Destroy(child.gameObject);
         }
 
-        int subCount = GetSubCount(stageNum);
+        int subCount = 3;
         for (int i = 1; i <= subCount; i++)
         {
             GameObject btn = Instantiate(buttonPrefab, buttonContainer);
@@ -24,19 +24,9 @@ public class SubStageUI : MonoBehaviour
             int s = stageNum, ss = i;
             btn.GetComponent<Button>().onClick.AddListener(() =>
             {
-                UnityEngine.SceneManagement.SceneManager.LoadScene($"Stage_{s}_{ss}"); //스테이지 이름 맞추어서 변경해주면 따라감
+                SceneManager.LoadScene($"stage{s}-{ss}"); //스테이지 이름 맞추어서 변경해주면 따라감
             });
         }
-    }
-
-    int GetSubCount(int stageNum)
-    {
-        return stageNum switch
-        {
-            1 => 3,
-            2 => 2,
-            _ => 1,
-        };
     }
 
     public void Close()
